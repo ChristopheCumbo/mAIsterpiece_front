d12d22f6fe9efaf2896d55c05a3acf92ec1fd2e8
NicoCaron <caronnicolas11@gmail.com>
2023-05-05 22:42:12 +0200

    FE_V1[Home Page] centrage de la grid, agrandissement du header et le footer et mise en place animation hover sur la grid

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,17 @@ import './style.scss';
 
 function Home() {
   return (
-    <div className="home__container">Home</div>
+    <div className="home__container">
+      <img className="home__container__img" src="https://www.zupimages.net/up/23/18/4tlv.jpg" alt="images de la semaine" />
+      <img className="home__container__img" src="https://www.zupimages.net/up/23/18/x3r4.jpg" alt="images de la semaine" />
+      <img className="home__container__img" src="https://www.zupimages.net/up/23/18/gb8y.jpg" alt="images de la semaine" />
+      <img className="home__container__img" src="https://www.zupimages.net/up/23/18/4a3q.jpg" alt="images de la semaine" />
+      <img className="home__container__img" src="https://www.zupimages.net/up/23/18/vi6a.jpg" alt="images de la semaine" />
+      <img className="home__container__img" src="https://www.zupimages.net/up/23/18/wasp.jpg" alt="images de la semaine" />
+      <img className="home__container__img" src="https://www.zupimages.net/up/23/18/gh6c.jpg" alt="images de la semaine" />
+      <img className="home__container__img" src="https://www.zupimages.net/up/23/18/8ptc.jpg" alt="images de la semaine" />
+      <img className="home__container__img" src="https://www.zupimages.net/up/23/18/qs7v.jpg" alt="images de la semaine" />
+    </div>
   );
 }
 
--- src/components/PictureOfTheWeek/index.js
+++ src/components/PictureOfTheWeek/index.js
@@ -1,11 +1,11 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import './style.scss';
 
 function PictureOfTheWeek() {
   return (
     <div className="pictureOfTheWeek">
-      <h1>PictureOfTheWeek</h1>
+      <h1>Image de la semaine</h1>
       <img className="pictureOfTheWeek__img" src="https://www.zupimages.net/up/23/18/4tlv.jpg" alt="images de la semaine" />
     </div>
   );
