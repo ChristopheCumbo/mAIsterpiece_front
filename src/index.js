cb1f78796fc7c2308e60817609d702d76e3eac3e
Benoit-Rolet <benoit.rolet@gmail.com>
2023-05-05 11:24:23 +0200

    FE_V1 : [installation] Ajout de redux et de redux-devtools-extension, squelette pour reducer, store et index.js à la racine

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@
 // React (aussi appelé DOM virtuel) avec le DOM Réel )
 import { createRoot } from 'react-dom/client';
 
+// import du nécessaire pour Redux
+import { Provider } from 'react-redux';
+
+/*
+-- BrowserRouter --
+https://reactrouter.com/docs/en/v6/api#browserrouter
+Composant de react-routeur-dom : il utilise l'HistoryAPI HTML5 pour surveiller l’historique des URL
+On englobe notre composant racine App avec <BrowserRouter>
+*/
+import { BrowserRouter } from 'react-router-dom';
+
 // composant racine
 import App from 'src/components/App';
+// import du store pour Redux
+import store from 'src/store';
 
 // == Render
-const rootReactElement = <App />;
+const rootReactElement = (
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
 
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
 const rootReactContainer = createRoot(document.getElementById('root'));
