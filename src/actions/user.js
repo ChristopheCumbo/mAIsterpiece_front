ea4d220bdf24638651a5baab1067d2f1cb8a3542
NicoCaron <caronnicolas11@gmail.com>
2023-05-09 21:53:20 +0200

    FE_V1: [Page d'inscription] Mise en place des champs controléssur la page d'inscription

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,10 +1,13 @@
 // Action type Login Page
 export const UPDATE_LOGIN_FORM_AUTH = 'UPDATE_LOGIN_FORM_AUTH';
 export const UPDATE_PASSWORD_AUTH = 'UPDATE_LOGIN_PASSWORD_AUTH';
-
 // Action type Register Page
+export const UPDATE_LOGIN_FORM_REGISTER = 'UPDATE_LOGIN_FORM_REGISTER';
+export const UPDATE_EMAIL_FORM_REGISTER = 'UPDATE_EMAIL_FORM_REGISTER';
+export const UPDATE_PASSWORD_REGISTER = 'UPDATE_PASSWORD_REGISTER';
+export const UPDATE_CONFIRM_PASSWORD_REGISTER = 'UPDATE_CONFIRM_PASSWORD_REGISTER';
 
-// - action creators
+// - action creators LoginPage
 
 /**
  * action creator qui renvoie l'action UPDATE_LOGIN_FORM_AUTH
@@ -23,6 +26,51 @@ export const actionUpdateLoginFormAuth = (newValue) => ({
  */
 export const actionUpdatePasswordFormAuth = (newValue) => ({
   type: UPDATE_PASSWORD_AUTH,
+  payload: {
+    newValue,
+  },
+});
+
+// - action creators RegisterPage
+
+/**
+ * action creator qui renvoie l'action UPDATE_LOGIN_FORM_REGISTER
+ * @return {Object} action
+ */
+export const actionUpdateLoginFormRegister = (newValue) => ({
+  type: UPDATE_LOGIN_FORM_REGISTER,
+  payload: {
+    newValue,
+  },
+});
+
+/**
+ * action creator qui renvoie l'action UPDATE_EMAIL_FORM_REGISTER
+ * @return {Object} action
+ */
+export const actionUpdateEmailFormRegister = (newValue) => ({
+  type: UPDATE_EMAIL_FORM_REGISTER,
+  payload: {
+    newValue,
+  },
+});
+/**
+ * action creator qui renvoie l'action UPDATE_PASSWORD_REGISTER
+ * @return {Object} action
+ */
+export const actionUpdatePasswordFormRegister = (newValue) => ({
+  type: UPDATE_PASSWORD_REGISTER,
+  payload: {
+    newValue,
+  },
+});
+
+/**
+ * action creator qui renvoie l'action UPDATE_CONFIRM_PASSWORD_REGISTER
+ * @return {Object} action
+ */
+export const actionUpdateConfirmPasswordFormRegister = (newValue) => ({
+  type: UPDATE_CONFIRM_PASSWORD_REGISTER,
   payload: {
     newValue,
   },
