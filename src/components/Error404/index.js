431f75c591abcd5d00dce585c015bcaa4190d07a
Benoit-Rolet <benoit.rolet@gmail.com>
2023-05-11 17:02:25 +0200

    FE_V1:[Page 404] Ajout image

--- src/components/Error404/index.js
+++ src/components/Error404/index.js
@@ -1,8 +1,11 @@
+// import style and picture
 import './style.scss';
 
 function Error404() {
   return (
-    <div className=".error404__container">Error404</div>
+    <div className="error404__container">
+      <img className="error404__img" src="https://zupimages.net/up/23/19/lbxp.jpg" alt="Erreur 404" />
+    </div>
   );
 }
 
