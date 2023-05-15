0ac71f3c8bccdd4a440974f356259f3c1b60550a
NicoCaron <caronnicolas11@gmail.com>
2023-05-15 21:34:21 +0200

    FE_V1: [Menu Burger Mobile] Mise en place d'un menu burger pour le mobile

--- src/components/AppHeader/AppHeaderMin/index.js
+++ src/components/AppHeader/AppHeaderMin/index.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import './style.scss';
 // import { Link } from 'react-feather';
--- src/components/AppHeader/NavBar/index.js
+++ src/components/AppHeader/NavBar/index.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 // import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
 import './style.scss';
+import { Menu, X } from 'react-feather';
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="navBar__container">
-      <Link to="/login">Connexion</Link> /&nbsp;
-      <Link to="/register">Inscription</Link>
+      <Link className="navBar__link" to="/login">Connexion</Link> <span>/&nbsp;</span>
+      <Link className="navBar__link" to="/register">Inscription</Link>
+
+      <div className="navBar__container__mobile">
+        <button type="button" onClick={toggleMenu}>
+          {isOpen ? <X /> : <Menu />}
+        </button>
+        <ul className={`${isOpen ? '' : 'li-close'}`}>
+          <li><Link to="/register">Inscription</Link></li>
+          <li> <Link to="/login">Connexion</Link></li>
+        </ul>
+      </div>
+
     </div>
   );
 }
